1c97ba5b2df8735df39e1180c09a43d2ec6dffa1
Duy0511 <91776994+Duy0511@users.noreply.github.com>
2022-07-07 22:41:04 +0700

    Add files via upload

---
+++ src/main/java/com/example/restaurantmanagement/WareHouseModel.java
@@ -0,0 +1,50 @@
+package com.example.demo11;
+
+public class WareHouseModel {
+    private String ingredient;
+    private int id;
+    private int number;
+    private double price;
+
+    public WareHouseModel(String ingredient, int id, int number, double price) {
+        this.ingredient = ingredient;
+        this.id = id;
+        this.number = number;
+        this.price = price;
+    }
+    public WareHouseModel(){
+
+    }
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+}
